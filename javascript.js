e1f468926d5a73bc5b537dbdbbc08c63b440079e
Jovaun_Henriques <jovaun@hotmail.com>
2024-02-12 21:40:55 -0330

    added tabs

--- javascript.js
+++ javascript.js
@@ -10,3 +10,22 @@ toggleBtn.onclick = function () {
     ?'fa-soild fs-xmark'
     :'fa-solid fa-bars'
 }
+function openTab(evt, tabName) {
+    var i, tabContent, tabButton;
+    tabContent = document.getElementsByClassName("tab");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+    tabButton = document.getElementsByClassName("tabButton");
+    for (i = 0; i < tabButton.length; i++) {
+      tabButton[i].className = tabButton[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  function submitForm() {
+    // Implement form submission logic here
+    // You can use JavaScript to handle form submission
+    console.log("Form submitted");
+  }

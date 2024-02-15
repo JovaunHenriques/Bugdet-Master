e7aa8ce7c9c3631abe6fbd3be18587cad0c5cdaf
Jovaun_Henriques <jovaun@hotmail.com>
2024-02-15 13:33:42 -0330

    removed unused code

--- javascript.js
+++ javascript.js
@@ -1,15 +1,3 @@
-const toggleBtn = document.querySelector('.toggle_btn')
-const toggleBtnIcon = document.querySelector('.toggle_btn i')
-const dropDownMenu = document.querySelector('.dropdown_Menu')
-
-toggleBtn.onclick = function () {
-    dropDownMenu.classList.toggle('open')
-    const isOpen = dropDownMenu.classList.contains('open')
-
-    toggleBtn.classList = isOpen
-    ?'fa-soild fs-xmark'
-    :'fa-solid fa-bars'
-}
 function openTab(evt, tabName) {
     var i, tabContent, tabButton;
     tabContent = document.getElementsByClassName("tab");
